4976516291e31075e9ca7a0e3175e53ab9254396
Michael Sumner <mdsumner@gmail.com>
2021-08-27 10:20:46 +1000

    uhg

--- data-raw/gdaltms.R
+++ data-raw/gdaltms.R
@@ -33,4 +33,5 @@ proj0 <- c(10, 11, 12, 13, 15, 16) + 5 ## plus 5 since added new google ones
 
 gdal_web_sources <- gdal_web_sources[-c(limit, bad, band1, proj0), ]
 usethis::use_data(gdal_web_sources)
+#usethis::use_data(gdal_web_sources, internal = TRUE)
 
