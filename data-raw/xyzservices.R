298267b7686ccfae84706330f3564b2d1d70d24f
Michael Sumner <mdsumner@gmail.com>
2021-09-11 20:01:16 +1000

    fix a bunch more unpack problems

--- data-raw/xyzservices.R
+++ data-raw/xyzservices.R
@@ -8,12 +8,32 @@ has_matrix <- function(x) any(purrr::map_lgl(x, is.matrix))
 as_tibble_with_matrix <- function(x) {
   if (has_matrix(x)) {
     x$bounds <- list(tibble::as_tibble(setNames(as.list(c(x$bounds)), c("xmin", "ymin", "xmax", "ymax"))))
-    tibble::as_tibble(x) %>% tidyr::unnest(bounds)
+    out <- tibble::as_tibble(x) %>% tidyr::unnest(bounds)
   } else {
-    tibble::as_tibble(x)
+    out <- tibble::as_tibble(x)
   }
+  ## flesh out the keys now
+  #scan("", 1)
+
+  ## these are exceptions to make, we don't have a data token for subdomain so just use "a" (but see below!)
+  ## server, some have abcd in $subdomains
+  out$s <- ""
+  if ("subdomains" %in% names(out)) {
+    out$s <- substr(out$subdomains, 1, 1)
+    browser()
+  }
+
+  out$x <- "{x}"
+  out$y <- "{y}"
+  out$z <- "{z}"
+  out$r <- ""  ## r is "@2x" for doubling the dimension of an image
+  out$url <- with(out, glue::glue(url))
+  out
 }
 
+#for ${s} subdomains we have to unpack these, I assume each is just one token not literally abcd or 1234
+# unique(xyzservices$subdomains)
+# [1] NA     "abcd" "1234" ""     "1"    "2"    "3"    "4"    "0123" "12"
 
 l <- list()
 for (i in seq_along(j)) {
@@ -28,3 +48,9 @@ for (i in seq_along(j)) {
 }
 
 xyzservices <- bind_rows(l)
+
+# there are 8 ways of specifying an API key
+#which(map_lgl(xyzservices, ~any(grepl("insert", .x))))
+#apikey     accessToken             key          apiKey          app_id        app_code subscriptionKey
+#17              20              25              28              30              31              42
+
